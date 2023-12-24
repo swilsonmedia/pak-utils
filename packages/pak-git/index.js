4b7933fdb49aa076db2e23ce0c0fcbe5b65fcd58
Stephen Wilson <486989+swilsonmedia@users.noreply.github.com>
2023-12-23 22:07:40 -0500

    New common git functionality added

--- packages/pak-git/index.js
+++ packages/pak-git/index.js
@@ -1,14 +1,1 @@
-import * as git from './lib/git.js';
-
-(async () => {
-    process.on('uncaughtException', (err) => {
-        console.log('uncaughtException', err.message);
-    });
-
-    process.on('unhandledRejection', (err) => {
-        console.log('unhandledRejection', err.message);
-    });
-
-    const response = await git.switchBranch('swilson/wilson');
-})();
-
+export * from './lib/git.js';
--- packages/pak-git/lib/git.js
+++ packages/pak-git/lib/git.js
@@ -3,39 +3,192 @@ import { promisify } from 'util';
 
 const execPromise = promisify(exec);
 
-export async function status(){
-    const cmd = `git status`;
-    const { stderr, stdout: message } = await execPromise(cmd);
+export async function addAll(){
+    const { stderr } = await execPromise('git add .');
+    
+    if(stderr){
+        throw new Error(stderr);
+    }
+}
+
+export async function commit(message){
+    if(!message){
+        throw new Error(`A message argument is required.`);
+    }
+
+    const { stderr, stdout } = await execPromise(`git commit -m "${message}"`);
+    
+    if(stderr){
+        throw new Error(stderr);
+    }
+    
+    return stdout;
+}
+
+export async function checkout(branch){
+    if(!branch){
+        throw new Error(`A branch argument is required.`);
+    }
+
+    const output = [];
+
+    const checkout = await execPromise(`git checkout -b ${branch}`);
+
+    output.push(checkout.stdout);
+
+    if(checkout.stderr){
+        if(!checkout.stderr.includes('Switched')){
+            throw checkout.stderr;
+        }
+
+        output.push(checkout.stderr);
+    }
+
+    return output.join('\n');
+}
+
+export async function deleteLocalBranch(branch){
+    if(!branch){
+        throw new Error(`A branch argument is required.`);
+    }
+
+    const { stderr, stdout } = await execPromise(`git branch -D ${branch}`);
 
     if(stderr){
         throw stderr;
     }
 
-    return {
-        cmd,
-        message,
-        changes: /changes/gi.test(message)
-    };
+    return stdout;
 }
 
-export async function switchBranch(branch){
+export async function deleteRemoteBranch(branch){
+    if(!branch){
+        throw new Error(`A branch argument is required.`);
+    }
+
+    const { stderr, stdout } = await execPromise(`git push -d origin ${branch}`);
+
+    if(stderr && !stderr.includes('deleted')){
+        throw stderr;
+    }
+
+    if(stderr && !stdout){
+        return stderr;
+    }
+
+    return stdout;
+}
+
+export async function logForAuthor(max=100){
+    const { stderr, stdout } = await execPromise(`git log -n ${max} --author=$(git config --get user.email) --pretty=oneline`);
+    
+    if(stderr){
+        throw new Error(stderr);
+    }
+    
+    return stdout;
+}
+
+export async function mergeSquashed(branch){
+    if(!branch){
+        throw new Error(`A branch argument is required.`);
+    }
+    
+    const { stderr, stdout } = await execPromise(`git merge --squash ${branch}`);
+    
+    if(stderr){
+        throw new Error(stderr);
+    }
+    
+    return stdout;
+}
+
+export async function merge(branch){
+    if(!branch){
+        throw new Error(`A branch argument is required.`);
+    }
+
+    const { stderr, stdout } = await execPromise(`git merge ${branch} -m "Merging ${branch} to branch"`);
+    
+    if(stderr){
+        throw new Error(stderr);
+    }
+    
+    return stdout;
+}
+
+export async function pull(){
+    const { stderr } = await execPromise('git pull');
+
+    if(stderr){
+        throw new Error(stderr);
+    }
+}
+
+export async function push(){
+    const { stderr } = await execPromise('git push');
+
+    if(stderr){
+        throw new Error(stderr);
+    }
+}
+
+export async function showCurrentBranch(){
+    const { stderr, stdout } = await execPromise(`git branch --show-current`);
+
+    if(stderr){
+        throw new Error(stderr);
+    }
+
+    return stdout;
+}
+
+export async function status(){
+    const { stderr, stdout } = await execPromise('git status');
+
+    if(stderr){
+        throw stderr;
+    }
+
+    return stdout;
+}
+
+export async function switchToBranch(branch){
     if(!branch){
         throw new Error(`A branch argument is required.`);
     }
         
-    const res = await status();
-
-        console.log(`Changes ${res.message}: ${res.changes}`);
-
-    // const { stderr, stdout } = await execPromise(`git switch ${branch}`);
+    const { stderr, stdout } = await execPromise(`git switch ${branch}`);
     
-    // if(/Switched|Already/.test(stderr) ){
-    //     return stderr;
-    // }
+    if(/Switched|Already/.test(stderr) ){
+        return stderr;
+    }
 
-    // if(stderr){
-    //     throw stderr;
-    // }
+    if(stderr){
+        throw stderr;
+    }
 
-    // return stdout;
+    return stdout;
+}
+
+export async function setUpstream(branch){
+    if(!branch){
+        throw new Error(`A branch argument is required.`);
+    }
+
+    const output = [];
+
+    const upstream =  await execPromise(`git push -u origin ${branch}`);
+
+    output.push(upstream.stdout);
+
+    if(upstream.stderr ){
+        if(!upstream.stderr.includes('remote: Create')){
+            throw upstream.stderr;
+        }
+
+        output.push(checkout.stderr);
+    }
+
+    return output.join('\n');
 }
