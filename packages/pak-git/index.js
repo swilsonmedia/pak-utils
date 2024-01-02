a700afe278fc51ae0a9b6903f1a9ce0ede0561b9
Stephen Wilson <stephen.wilson@covetrus.com>
2024-01-02 15:13:23 -0500

    Refactor pak-vsc

--- packages/pak-git/index.js
+++
@@ -1,8 +0,0 @@
-import { exec } from 'child_process';
-import { promisify } from 'util';
-import makeGit from './lib/git.js';
-
-const execPromise = promisify(exec);
-const git = makeGit(execPromise);
-
-export default git;
